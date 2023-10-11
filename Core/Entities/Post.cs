a5c72885dfce4221ceee5e6c90c197fc970662d8
Jesús Flores <jesusadrian1953@outlook.com>
2023-10-11 12:31:17 -0600

    Added migration for relationship between user and post entities

--- Core/Entities/Post.cs
+++ Core/Entities/Post.cs
@@ -12,5 +12,8 @@ namespace Core.Entities
         public string Title { get; set; }
         public string Description { get; set; }
 
+        public int UserId { get; set; }
+        public User User { get; set; }
+
     }
 }
---
+++ Infra/Migrations/20231011182915_PostUserRelationShip.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class PostUserRelationShip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Posts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_AspNetUsers_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Posts");
+        }
+    }
+}
