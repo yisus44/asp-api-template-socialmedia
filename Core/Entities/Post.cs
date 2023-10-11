7406b12adbe30feadc452e8ca4b94a872bee0c3b
Jesús Flores <jesusadrian1953@outlook.com>
2023-10-11 13:48:03 -0600

    Added comment migration

--- Core/Entities/Post.cs
+++ Core/Entities/Post.cs
@@ -14,6 +14,7 @@ namespace Core.Entities
 
         public int UserId { get; set; }
         public User User { get; set; }
+        public List<Comment> Comments { get; set; }
 
     }
 }
--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -6,5 +6,6 @@ namespace Core.Entities
     public class User : IdentityUser<int>
     {
       public List<Post> Posts { get; set; }
+      public List<Comment> Comments { get; set; }
     }
 }
